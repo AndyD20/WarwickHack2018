0ab39688009776ee80f98dafa709fcd5c5981553
Andrew Delaney <adelaney2011@hotmail.co.uk>
2018-02-22 13:55:58 +0000

    Work done during WarwickHack

---
+++ WarwickHack/Assets/_Scripts/PlatformMove.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour {
+
+    public bool moveY = false;
+    public float moveDistance;
+    public float speed;
+    public float delay = 0;
+
+    private float limLow;
+    private float limHigh;
+    private bool moveDownLeft;
+
+	// Use this for initialization
+	void Start () {
+
+        if (moveY) { limLow = transform.position.y - moveDistance; limHigh = transform.position.y + moveDistance; }
+        else { limLow = transform.position.x - moveDistance; limHigh = transform.position.x + moveDistance; }
+
+        moveDownLeft = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(movePlatform());
+    }
+
+    IEnumerator movePlatform()
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (moveY && moveDownLeft)
+        {
+            if (transform.position.y - speed >= limLow)
+            {
+                transform.Translate(0, speed * -1, 0);
+            }
+            else
+            {
+                moveDownLeft = false;
+            }
+        }
+
+        else if (moveY && !moveDownLeft)
+        {
+            if (transform.position.y + speed <= limHigh)
+            {
+                transform.Translate(0, speed, 0);
+            }
+            else
+            {
+                moveDownLeft = true;
+            }
+        }
+
+        if (!moveY && moveDownLeft)
+        {
+
+            if (transform.position.x - speed >= limLow)
+            {
+                transform.Translate(speed * -1, 0, 0);
+            }
+            else
+            {
+                moveDownLeft = false;
+            }
+
+        }
+
+        else if (!moveY && !moveDownLeft)
+        {
+            if (transform.position.x + speed <= limHigh)
+            {
+                transform.Translate(speed, 0, 0);
+            }
+            else
+            {
+                moveDownLeft = true;
+            }
+        }
+    }
+}
---
+++ WarwickHack/Assets/_Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    public float health = 5;
+    public Transform heart;
+    public Transform emptyHeart;
+    public GameObject healthBar;
+
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    protected bool attacking;
+
+    // Use this for initialization
+    void Awake () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer> (); 
+        animator = GetComponent<Animator> ();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis ("Horizontal");
+
+        if (Input.GetButtonDown ("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        } else if (Input.GetButtonUp ("Jump")) 
+        {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        if (move.x > 0.01f)
+        {
+            if(spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+        } 
+        else if (move.x < -0.01f)
+        {
+            if(spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+
+        
+        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+
+        if (Input.GetMouseButtonDown(0)) { attacking = true; velocity.x = 0; }
+        else attacking = false;
+
+        animator.SetBool("attacking", attacking);
+
+        animator.SetBool("grounded", grounded);
+
+        targetVelocity = move * maxSpeed;
+    }
+
+    private void UpdateHealth()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "platform")
+        {
+            transform.parent = collision.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "platform")
+        {
+            transform.parent = null;
+        }
+    }
+}
